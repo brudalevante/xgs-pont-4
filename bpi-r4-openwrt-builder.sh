bca001a1e0ae473aaef2ba15cbe237b41d96bb56
brudalevante <continentalwl@gmail.com>
2025-08-11 16:51:43 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -20,7 +20,7 @@ rm -rf mtk-openwrt-feeds
 rm -rf tmp_comxwrt
 
 echo "==== 2. CLONA REPOSITORIOS ===="
-git clone --branch openwrt-24.10 https://github.com/brudalevante/openwrt-1.git openwrt || true
+git clone --branch openwrt-24.10 https://github.com/brudalevante/openwrt-2.git openwrt || true
 cd openwrt; git checkout 41a49ccb47a4fddac54fac58542ee6dfb0691f79; cd -;	# uhttpd: update to Git HEAD (2025-07-06)
 
 git clone https://github.com/brudalevante/led-mtk.git mtk-openwrt-feeds || true
