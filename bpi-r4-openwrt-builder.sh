afe67ee6f7070fcd0bb3d42e21ccde93d8c6d9e6
brudalevante <continentalwl@gmail.com>
2025-08-12 16:04:55 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -81,10 +81,10 @@ cp -v my_files/board.json openwrt/package/base-files/files/etc/board.json
 # CREA EL FEEDS.CONF PERSONALIZADO AQUÍ
 echo "==== CREANDO feeds.conf PERSONALIZADO ===="
 cat > openwrt/feeds.conf <<EOF
-src-git packages https://git.openwrt.org/feed/packages.git^c7d1a8c1ae976bd0ad94a351d82ee8fbf16a81f0
-src-git luci https://git.openwrt.org/project/luci.git^d6b13f648339273facc07b173546ace459c1cabe
-src-git routing https://git.openwrt.org/feed/routing.git^85d040f28c21c116c905aa15a66255dde80336e7
-src-git telephony https://git.openwrt.org/feed/telephony.git^2a4541d46199ac96fac214d02c908402831c4dc6
+src-git packages https://git.openwrt.org/feed/packages.git;openwrt-24.10
+src-git luci https://git.openwrt.org/project/luci.git;openwrt-24.10
+src-git routing https://git.openwrt.org/feed/routing.git;openwrt-24.10
+src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-24.10
 src-link mtk_openwrt_feed /home/vboxuser/xgs-pont-4/mtk-openwrt-feeds
 EOF
 # ============================================================
