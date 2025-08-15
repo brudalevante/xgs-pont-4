b1e91e9e982112ff0cd99cf4f1b3895e7dcc7bae
brudalevante <continentalwl@gmail.com>
2025-08-15 13:00:34 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 set -e
 
@@ -6,7 +6,7 @@ echo "==== 1. LIMPIEZA ===="
 rm -rf openwrt mtk-openwrt-feeds tmp_comxwrt
 
 echo "==== 2. CLONA TUS REPOS PERSONALES ===="
-git clone --branch main https://github.com/brudalevante/6.6.100-openwrt.git openwrt || true
+git clone --branch openwrt-24.10 https://github.com/brudalevante/6.6.100-openwrt.git openwrt || true
 cd openwrt
 git checkout 4941509f573676c4678115a0a3a743ef78b63c17
 cd ..
@@ -53,10 +53,10 @@ cd openwrt
 rm -rf feeds/
 cat feeds.conf.default
 
-echo "==== 8. COPIA LA CONFIGURACIÃ“N BASE (mm_perf.config) ===="
+echo "==== 8. COPIA LA CONFIGURACIÓN BASE (mm_perf.config) ===="
 cp -v ../configs/mm_perf.config .config
 
-echo "==== 9. COPIA TU CONFIGURACIÃ“N PERSONALIZADA AL DEFCONFIG DEL AUTOBUILD ===="
+echo "==== 9. COPIA TU CONFIGURACIÓN PERSONALIZADA AL DEFCONFIG DEL AUTOBUILD ===="
 cp -v ../configs/mm_perf.config ../mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
 
 echo "==== 10. ACTUALIZA E INSTALA FEEDS ===="
@@ -66,16 +66,16 @@ echo "==== 10. ACTUALIZA E INSTALA FEEDS ===="
 echo "==== 11. RESUELVE DEPENDENCIAS ===="
 make defconfig
 
-echo "==== 12. VERIFICACIÃ“N FINAL ===="
+echo "==== 12. VERIFICACIÓN FINAL ===="
 for pkg in \
   fakemesh autoreboot cpu-status temp-status dawn2 dawn usteer2 wireguard
 do
   grep $pkg .config || echo "NO aparece $pkg en .config"
 done
 
-grep "CONFIG_PACKAGE_kmod-wireguard=y" .config || echo "ATENCIÃ“N: kmod-wireguard NO estÃ¡ marcado"
-grep "CONFIG_PACKAGE_wireguard-tools=y" .config || echo "ATENCIÃ“N: wireguard-tools NO estÃ¡ marcado"
-grep "CONFIG_PACKAGE_luci-proto-wireguard=y" .config || echo "ATENCIÃ“N: luci-proto-wireguard NO estÃ¡ marcado"
+grep "CONFIG_PACKAGE_kmod-wireguard=y" .config || echo "ATENCIÓN: kmod-wireguard NO está marcado"
+grep "CONFIG_PACKAGE_wireguard-tools=y" .config || echo "ATENCIÓN: wireguard-tools NO está marcado"
+grep "CONFIG_PACKAGE_luci-proto-wireguard=y" .config || echo "ATENCIÓN: luci-proto-wireguard NO está marcado"
 
 echo "==== 13. EJECUTA AUTOBUILD ===="
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 log_file=make
